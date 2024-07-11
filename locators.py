8bb51c98e0daa282a838e7d9fc2b0c0b0352ad57
Evgeniy Soldatkin <ggsjonny@yandex.ru>
2024-07-11 12:29:49 +0100

    Добавление файлов

--- locators.py
+++ locators.py
@@ -1,45 +1,45 @@
 from selenium.webdriver.common.by import By
 
 # локаторы для регистрации
-class RegistrationLocators:
-    NAME_FIELD = (By.XPATH, "//input[@name='name']")
-    EMAIL_FIELD = (By.XPATH, "//fieldset[2]/div/div/input[@name='name']")
-    PASSWORD_FIELD = (By.XPATH, "//input[@name='Пароль']")
-    REGISTRATION_BUTTON = (By.XPATH, "//button[text()='Зарегистрироваться']")
-    ERROR_MESSAGE = (By.XPATH, "//p[text()='Некорректный пароль']")
-    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
+class registrationlocators:
+    name_field = (By.XPATH, "//input[@name='name']")
+    email_field = (By.XPATH, "//fieldset[2]/div/div/input[@name='name']")
+    password_field = (By.XPATH, "//input[@name='Пароль']")
+    registration_button = (By.XPATH, "//button[text()='Зарегистрироваться']")
+    error_message = (By.XPATH, "//p[text()='Некорректный пароль']")
+    login_button = (By.XPATH, "//button[text()='Войти']")
 
 # локаторы для активации
-class LoginLocators:
-    LOGIN_TO_ACCOUNT_BUTTON = (By.XPATH, "//button[text()='Войти в аккаунт']")
-    LOGIN_FIELD = (By.XPATH, "//input[@name='name']")
-    PASSWORD_FIELD = (By.XPATH, "//input[@name='Пароль']")
-    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
-    PERSONAL_ACCOUNT = (By.XPATH, "//p[text()='Личный Кабинет']")
-    ORDER_BUTTON = (By.XPATH, "//button[text()='Оформить заказ']")
-    LOGIN_LINK = (By.XPATH, "//a[text()='Войти']")
+class loginlocators:
+    login_to_account_button = (By.XPATH, "//button[text()='Войти в аккаунт']")
+    login_field = (By.XPATH, "//input[@name='name']")
+    password_field = (By.XPATH, "//input[@name='Пароль']")
+    login_button = (By.XPATH, "//button[text()='Войти']")
+    personal_account = (By.XPATH, "//p[text()='Личный Кабинет']")
+    order_button = (By.XPATH, "//button[text()='Оформить заказ']")
+    login_link = (By.XPATH, "//a[text()='Войти']")
 
 # локаторы для переходов
-class GoPageLocators:
-    LOGIN_TO_ACCOUNT_BUTTON = (By.XPATH, "//button[text()='Войти в аккаунт']")
-    LOGIN_FIELD = (By.XPATH, "//input[@name='name']")
-    PASSWORD_FIELD = (By.XPATH, "//input[@name='Пароль']")
-    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
-    ORDER_BUTTON = (By.XPATH, "//button[text()='Оформить заказ']")
-    PERSONAL_ACCOUNT = (By.XPATH, "//p[text()='Личный Кабинет']")
-    PROFILE = (By.XPATH, "//a[text()='Профиль']")
-    CONSTRUCTOR = (By.XPATH, "//p[text()='Конструктор']")
-    LOGO = (By.XPATH, "//*[@id='root']/div/header/nav/div")
-    EXIT_BUTTON = (By.XPATH, "//button[text()='Выход']")
+class gopagelocators:
+    login_to_account_button = (By.XPATH, "//button[text()='Войти в аккаунт']")
+    login_field = (By.XPATH, "//input[@name='name']")
+    password_field = (By.XPATH, "//input[@name='Пароль']")
+    login_button = (By.XPATH, "//button[text()='Войти']")
+    order_button = (By.XPATH, "//button[text()='Оформить заказ']")
+    personal_account = (By.XPATH, "//p[text()='Личный Кабинет']")
+    profile = (By.XPATH, "//a[text()='Профиль']")
+    constructor = (By.XPATH, "//p[text()='Конструктор']")
+    logo = (By.XPATH, "//*[@id='root']/div/header/nav/div")
+    exit_button = (By.XPATH, "//button[text()='Выход']")
 
 # локаторы для контструктора
-class ConstructorLocators:
-    LOGIN_TO_ACCOUNT_BUTTON = (By.XPATH, "//button[text()='Войти в аккаунт']")
-    LOGIN_FIELD = (By.XPATH, "//input[@name='name']")
-    PASSWORD_FIELD = (By.XPATH, "//input[@name='Пароль']")
-    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
-    PERSONAL_ACCOUNT = (By.XPATH, "//p[text()='Личный Кабинет']")
-    ORDER_BUTTON = (By.XPATH, "//button[text()='Оформить заказ']")
-    SAUCES = (By.XPATH, '//*[@id="root"]/div/main/section[1]/div[1]/div[2]/span')
-    BUNS = (By.XPATH, '//*[@id="root"]/div/main/section[1]/div[1]/div[1]/span')
-    FILLINGS = (By.XPATH, '//*[@id="root"]/div/main/section[1]/div[1]/div[3]/span')
+class constructorlocators:
+    login_to_account_button = (By.XPATH, "//button[text()='Войти в аккаунт']")
+    login_field = (By.XPATH, "//input[@name='name']")
+    password_field = (By.XPATH, "//input[@name='Пароль']")
+    login_button = (By.XPATH, "//button[text()='Войти']")
+    personal_account = (By.XPATH, "//p[text()='Личный Кабинет']")
+    order_button = (By.XPATH, "//button[text()='Оформить заказ']")
+    sauces = (By.XPATH, '//*[@id="root"]/div/main/section[1]/div[1]/div[2]/span')
+    buns = (By.XPATH, '//*[@id="root"]/div/main/section[1]/div[1]/div[1]/span')
+    fillings = (By.XPATH, '//*[@id="root"]/div/main/section[1]/div[1]/div[3]/span')
